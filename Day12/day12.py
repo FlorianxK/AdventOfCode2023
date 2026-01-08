0c7b6aed5584c29ac2dd5681d7c6cc19dac20543
FlorianxK <Florian.Koehler@jokonet.de>
2026-01-08 18:02:54 +0100

    day12 finished

--- Day12/day12.py
+++ Day12/day12.py
@@ -1,56 +1,80 @@
-import itertools
 from typing import *
 
 def dayTwelve():
     res = 0
-    def match(curr,groups):
-        pattern = []
-        temp = 0
-        curr.append('.')
-        for c in curr:
-            if c == '#':
-                temp += 1
-            else:
-                if temp > 0:
-                    pattern.append(temp)
-                    temp = 0
+    cache = {}
 
-        if groups == pattern:
-            return True
-        else:
-            return False
+    def count(curr,groups):
+        if curr == "":
+            return 1 if groups == () else 0
+        if groups == ():
+            return 0 if '#' in curr else 1
+        
+        key = (curr,groups)
+
+        if key in cache:
+            return cache[key]
+
+        tempRes = 0
+        if curr[0] in ".?":
+            tempRes += count(curr[1:],groups)
+        
+        if curr[0] in "#?":
+            if groups[0] <= len(curr) and '.' not in curr[:groups[0]] and (groups[0] == len(curr) or curr[groups[0]] != '#'):
+                tempRes += count( curr[groups[0] + 1:], groups[1:] )
+        
+        cache[key] = tempRes
+        return tempRes
+    
+    #read
+    with open("Day12/12_2.txt") as file:
+        for line in file:
+            arr,r = line.rstrip().split(' ')
+            groups = tuple([int(x) for x in r.split(',')])
+            
+            res += count(arr, groups)
+    return res
+  
+def dayTwelve2():
+    res = 0
+    cache = {}
+
+    def count(curr,groups):
+        if curr == "":
+            return 1 if groups == () else 0
+        if groups == ():
+            return 0 if '#' in curr else 1
+        
+        key = (curr,groups)
+
+        if key in cache:
+            return cache[key]
+
+        tempRes = 0
+        if curr[0] in ".?":
+            tempRes += count(curr[1:],groups)
+        
+        if curr[0] in "#?":
+            if groups[0] <= len(curr) and '.' not in curr[:groups[0]] and (groups[0] == len(curr) or curr[groups[0]] != '#'):
+                tempRes += count( curr[groups[0] + 1:], groups[1:] )
+        
+        cache[key] = tempRes
+        return tempRes
     
     #read
     with open("Day12/12_2.txt") as file:
         for line in file:
             l,r = line.rstrip().split(' ')
-            missing = 0
-            arr = list(l)
-            groups = [int(x) for x in r.split(',')]
-            for v in arr:
-                if v == '?':
-                    missing += 1
-
-            for comb in itertools.product(['.','#'], repeat=missing):
-                index = 0
-                curr = arr.copy()
-                for i in range(len(curr)):
-                    if curr[i] == '?':
-                        curr[i] = comb[index]
-                        index += 1
-                if match(curr,groups):
-                    res += 1
-    return res
+            arr = "?".join([l]*5)
+            groups = tuple([int(x) for x in r.split(',')]*5)
             
-    
-
-def dayTwelve2():
-    pass
+            res += count(arr, groups)
+    return res
 
 def main():
     print("Hallo")
     print(dayTwelve(), "ist die Lösung von Teil 1")
-    #print(dayTwelve2(), "ist die Lösung von Teil 2")
+    print(dayTwelve2(), "ist die Lösung von Teil 2")
      
 if __name__=="__main__":
     main()
