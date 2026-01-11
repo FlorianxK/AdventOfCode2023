f1bd4ffdaa0149b2a0a93e237e2ffcaf345eb8ff
FlorianxK <Florian.Koehler@jokonet.de>
2026-01-11 15:33:36 +0100

    day15 finished

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import *
 
 def dayFifteen():
@@ -22,7 +23,53 @@ def dayFifteen():
     return res
 
 def dayFifteen2():
-    pass
+    res = 0
+
+    def hashAlgo(word):
+        val = 0
+        for c in word:
+            asc = ord(c)
+            val += asc
+            val *= 17
+            val = val%256
+        return val
+
+    #read
+    with open("Day15/15_2.txt") as file:
+        for line in file:
+            arr = line.strip().split(',')
+    
+    d = defaultdict(list)
+    vals = defaultdict(int)
+    for word in arr:
+        if word[-1] == '-':
+            l = word[:len(word)-1]
+            key = tuple(l)
+            box = hashAlgo(l)
+            if key in d[box]:
+                if len(d[box]) == 1:
+                    del d[box]
+                else:
+                    d[box].remove(key)
+                del vals[key]
+        else:
+            l,r = word.split('=')
+            key = tuple(l)
+            r = int(r)
+            box = hashAlgo(list(l))
+
+            if key in d[box]:
+                vals[key] = r
+            else:
+                d[box].append(key)
+                vals[key] = r
+
+    #calc
+    for k,v in d.items():
+        for i in range(len(v)):
+            res += (k+1) * (i+1) * vals[v[i]]
+
+    return res
 
 def main():
     print("Hallo")
