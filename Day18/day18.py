c285d09eb0eaa028078868e6971553e91489b99a
FlorianxK <Florian.Koehler@jokonet.de>
2026-01-24 18:45:24 +0100

    day18 finished

--- Day18/day18.py
+++ Day18/day18.py
@@ -1,98 +1,49 @@
-from collections import deque
 from typing import *
 
 def dayEighteen():
-    input = []
-    m,n = 0,0
-    l,r,u,d = 0,0,0,0
-    lm,rm,um,dm = 0,0,0,0
+    points = [(0,0)]
+    dirs = {'U': (-1,0),'D': (1,0),'L': (0,-1),'R': (0,1)}
+    border = 0
     #read
-    with open("Day18/18.txt") as file:
+    with open("Day18/18_2.txt") as file:
         for line in file:
-            cdir,steps,hexa = line.split(' ')
+            cdir,steps,_ = line.split()
             steps = int(steps)
-            input.append([cdir,steps,hexa])
-            if cdir == 'R':
-                r += steps
-                l = max(0,l-steps)
-            elif cdir == 'L':
-                l += steps
-                r = max(0,r-steps)
-            elif cdir == 'D':
-                d += steps
-                u = max(0,u-steps)
-            elif cdir == 'U':
-                u += steps
-                d = max(0,d-steps)
-            lm = max(lm,l)
-            rm = max(rm,r)
-            dm = max(dm,d)
-            um = max(um,u)
-            
-        m = max(um,dm)
-        n = max(lm,rm)
-        m += 1
-        n += 1
-    arr = [['.']*n for _ in range(m)]
-    arr[0][0] = '#'
-    ring = 0
-    i,j = 0,0
-    for cdir,steps,_ in input:
-        ring += steps
-        if cdir == 'R':
-            dx,dy = (0,1)
-        elif cdir == 'L':
-            dx,dy = (0,-1)
-        elif cdir == 'D':
-            dx,dy = (1,0)
-        elif cdir == 'U':
-            dx,dy = (-1,0)
-        
-        for _ in range(steps):
-            i,j = i+dx,j+dy
-            arr[i][j] = '#'
+            dx,dy = dirs[cdir]
+            x,y = points[-1]
+            points.append( (x+dx*steps,y+dy*steps) )
+            border += steps
 
-    seen = set()
-    def bfs(i,j):
-        q = deque([(i,j)])
-        seen.add((i,j))
-        seenInRound = set()
-        seenInRound.add( (i,j) )
-        inside = True
-        while q:
-            curr = q.popleft()
-            i,j = curr
-            for dx,dy in [(0,1),(0,-1),(1,0),(-1,0)]:
-                nx,ny = dx+i,dy+j
-                if 0<=nx<m and 0<=ny<n:
-                    if (nx,ny) in seen or arr[nx][ny] != '.':
-                        continue
+    res = 0
+    for i in range(len(points)):
+        res += points[i][0]*( points[i-1][1]-points[(i+1)%len(points)][1] )
+    res = abs(res)//2
+    inside = res - border//2 + 1
 
-                    seen.add( (nx,ny) )
-                    seenInRound.add( (nx,ny) )
-                    q.append((nx,ny))
-                else:
-                    inside = False
-
-        if inside:
-            for a,b in seenInRound:
-                arr[a][b] = '#'
-            return len(seenInRound)
-        else:
-            return 0
-
-    inside = 0
-    for i in range(m):
-        for j in range(n):
-            if arr[i][j] == '.' and (i,j) not in seen:
-                inside += bfs(i,j)
-
-    for a in arr:
-        print(''.join(a))
-    return ring+inside
+    return inside+border
 
 def dayEighteen2():
-    pass
+    points = [(0,0)]
+    dirs = {'3': (-1,0),'1': (1,0),'2': (0,-1),'0': (0,1)}
+    border = 0
+    #read
+    with open("Day18/18_2.txt") as file:
+        for line in file:
+            _,_,hexa = line.split()
+            cdir = hexa[-2]
+            dx,dy = dirs[cdir]
+            steps = int(hexa[2:len(hexa)-2],16)
+            x,y = points[-1]
+            points.append( (x+dx*steps,y+dy*steps) )
+            border += steps
+
+    res = 0
+    for i in range(len(points)):
+        res += points[i][0]*( points[i-1][1]-points[(i+1)%len(points)][1] )
+    res = abs(res)//2
+    inside = res - border//2 + 1
+
+    return inside+border
 
 def main():
     print("Hallo")
