e69cc5b87a22e2f6ea62fd41668e203f61086a14
FlorianxK <Florian.Koehler@jokonet.de>
2026-01-28 12:13:27 +0100

    day7 finished

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,76 +1,86 @@
-from collections import defaultdict
 from typing import *
 
 def daySeven():
-    cards = []
-    bids = []
-    #read
-    with open("Day7/7.txt") as file:
-        for line in file:
-            l,r = line.rstrip().split(' ')
-            d = defaultdict(int)
-            for c in l:
-                if c == 'T':
-                    d[10] += 1
-                elif c == 'J':
-                    d[11] += 1
-                elif c == 'Q':
-                    d[12] += 1    
-                elif c == 'K':
-                    d[13] += 1
-                elif c == 'A':
-                    d[14] += 1
-                else:
-                    d[int(c)] += 1
+    
+    letter_map = {'T':'A','J':'B','Q':'C','K':'D','A':'E'}
 
-            cards.append(d)
-            bids.append(int(r))
-
-    for c,b in zip(cards,bids):
-        print(dict(c),b)
-    print()
-
-    temp = defaultdict(list)
-    c:defaultdict
-    for i,c in enumerate(cards):
-        sVals = list(sorted(c.values(),reverse=True))
-        if max(c.values()) == 5:
-            temp[1].append(i)
-        elif max(c.values()) == 4:
-            temp[2].append(i)
-        elif len(c.values()) == 2 and sVals[0] == 3 and sVals[1] == 2:
-            temp[3].append(i)
-        elif max(c.values()) == 3:
-            temp[4].append(i)
-        elif max(c.values()) == 2:
-            if sVals[0] == 2 and sVals[1] == 2:
-               temp[5].append(i)
-            else:
-                temp[6].append(i)
-        elif max(c.values()) == 1:
-            temp[7].append(i)
-    temp = dict(sorted(temp.items()))
+    def classify(hand):
+        counts = [hand.count(card) for card in hand]
+        if 5 in counts:
+            return 6
+        if 4 in counts:
+            return 5
+        if 3 in counts:
+            if 2 in counts:
+                return 4
+            return 3
+        if counts.count(2) == 4:
+            return 2
+        if 2 in counts:
+            return 1
+        return 0
 
     def strength(hand):
-        cards[hand]
-
-    #compare is wrong because it doesnt look at highest card
-    #sort function that looks at hashmap highest val then second highest
-    for k,v in temp.items():
-        v.sort(key = lambda v: strength(v))
+        return (classify(hand), [letter_map.get(card,card) for card in hand])
+    
+    cards = []
+    with open("Day7/7_2.txt") as file:
+        for line in file:
+            l,r = line.split()
+            cards.append( (l,int(r)) )
+    
+    cards.sort( key = lambda x: strength(x[0]) )
 
     res = 0
-    index = 5
-    for v in temp.values():
-        for val in v:
-            print(bids[val])
-            res += index*bids[val]
-            index -= 1
-
+    for rank, (_,bid) in enumerate(cards,1):
+        res += rank*bid
     return res
 
 def daySeven2():
-    pass
+    letter_map = {'T':'A','J':'.','Q':'C','K':'D','A':'E'}
+
+    def score(hand):
+        counts = [hand.count(card) for card in hand]
+        if 5 in counts:
+            return 6
+        if 4 in counts:
+            return 5
+        if 3 in counts:
+            if 2 in counts:
+                return 4
+            return 3
+        if counts.count(2) == 4:
+            return 2
+        if 2 in counts:
+            return 1
+        return 0
+
+    def replace(hand):
+        if hand == "":
+            return [""]
+        return [x+y
+                for x in ("23456789TQKA" if hand[0] == 'J' else hand[0])
+                for y in replace(hand[1:])]
+
+    def classify(hand):
+        return max( map(score,replace(hand)) )
+
+    def strength(hand):
+        return (classify(hand), [letter_map.get(card,card) for card in hand])
+    
+    cards = []
+    with open("Day7/7_2.txt") as file:
+        for line in file:
+            l,r = line.split()
+            cards.append( (l,int(r)) )
+    
+    cards.sort( key = lambda x: strength(x[0]) )
+
+    res = 0
+    for rank, (_,bid) in enumerate(cards,1):
+        res += rank*bid
+    return res
+
 
 def main():
     print("Hallo")
